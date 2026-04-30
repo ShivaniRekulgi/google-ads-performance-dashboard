fce7e012510f44ac0db081909087ba0e18fdd9fb
Shivani Rekulgi <70626818+ShivaniRekulgi@users.noreply.github.com>
2026-04-30 17:53:17 +0530

    Refactor data cleaning script and enhance readability-added comments
    Refactored data cleaning process, added comments for clarity, and ensured numeric conversions are handled correctly.

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,26 +1,31 @@
 import pandas as pd
 import re
+#loading data
 df = pd.read_excel("C:/Users/rs38129/Desktop/BI/Google Ads/GoogleAds_DataAnalytics_Sales_Uncleaned.xlsx")
-#print(df.head())
+print(df.head())
 df.info()
 df.describe()
 df.isnull().sum()
+#cleaning the Cost column to remove unwanted symbols and convert the data type to numeric
 df['Cost']=df['Cost'].astype(str)
 df['Cost']=df['Cost'].str.replace('₹','',regex=False)
 df['Cost']=df['Cost'].str.replace('$','',regex=False)
 df['Cost']=df['Cost'].str.replace(',','',regex=False)
 df['Cost']=pd.to_numeric(df['Cost'],errors='coerce')
+#covert date column to datetime type
 df['Ad_Date']=pd.to_datetime(df['Ad_Date'], errors='coerce')
+#stripping extra spaces and making the text more readable  
 df['Device']=df['Device'].str.strip().str.title()
 df['Location']=df['Location'].str.strip().str.title()
 df['Location']=df['Location'].str.replace({'Hyderbad':'Hyderabad','Hydrebad':'Hyderabad'})
+#replacing null or missing values with 0
 df['Cost']=df['Cost'].fillna(0)
 df['Clicks']=df['Clicks'].fillna(0)
+df['Conversions']=df['Conversions'].fillna(0)
 df['Campaign_Name']=df['Campaign_Name'].str.strip()
 df['Campaign_Name'] = df['Campaign_Name'].apply(
     lambda x: re.sub(r'([a-z])([A-Z])', r'\1 \2', x)
 )
-df['Conversions']=df['Conversions'].fillna(0)
 df['CTR']=df['Clicks']/df['Impressions'].replace(0, None)
 df['Conversion_Rate_Calc']=df['Conversions']/df['Clicks']
 df['Cost']=pd.to_numeric(df['Cost'], errors='coerce')
@@ -39,4 +44,5 @@ df['Cost_Per_Conversion'] = df['Cost'] / df['Conversions']
 df['Cost_Per_Conversion'] = pd.to_numeric(df['Cost_Per_Conversion'], errors='coerce')
 df['ROI'] = (df['Sale_Amount'] - df['Cost']) / df['Cost']
 df['ROI'] = pd.to_numeric(df['ROI'], errors='coerce')
+#exporting dataframe to excel
 df.to_excel("C:/Users/rs38129/Desktop/BI/Google Ads/Cleaned_ads_data.xlsx", index=False)
